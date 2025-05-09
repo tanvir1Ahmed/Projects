ed0ba8aba77f691b8a239a2ece5d8c862d3f2feb
tanvir1Ahmed <tanvir.ahmed.ta.khan@gmail.com>
2025-05-09 17:10:57 +0600

    Library Management System

---
+++ Library Management System/Library Management System/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+
+namespace Library_Management_System.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/library/record")]
+        public async Task<HttpResponseMessage> ShowRecord()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://localhost:44306/api/record/details");
+            if (!response.IsSuccessStatusCode)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            var list = JsonConvert.DeserializeObject(content);
+            return Request.CreateResponse(HttpStatusCode.OK, list);
+        }
+    }
+}
